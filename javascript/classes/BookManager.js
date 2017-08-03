06653f3d1c88f071b280a8a86fd528c55626d1ba
Mikhail Cheberya <mikhail@idope.ru>
2017-08-03 23:54:24 +0300

    добавление книги в список (пока без LocalStorage)

--- javascript/classes/BookManager.js
+++ javascript/classes/BookManager.js
@@ -1,4 +1,5 @@
 import { Fields } from '../constants';
+import escapeHtml from '../utils/escapeHtml';
 
 export default class BookManager {
     constructor( node ) {
@@ -6,6 +7,8 @@ export default class BookManager {
         this.$fields = this.getFieldNodes();
         this.$submit = node.querySelector('.js-book-submit');
         this.$list = node.querySelector('.js-book-list');
+
+        this.idIterator = 0;
 
         this.attachEvents();
     }
@@ -18,8 +21,34 @@ export default class BookManager {
         return nodes;
     }
 
+    getIterationId() {
+        this.idIterator++;
+        // обновление в LocalStorage
+        return this.idIterator;
+    }
+
+    createItemNode() {
+        const bookTitle = escapeHtml( this.$fields.title.value );
+        const bookAuthor = escapeHtml( this.$fields.author.value );
+
+        let node = document.createElement('li');
+            node.className = 'list__item';
+            node.setAttribute('data-id', this.getIterationId() );
+            node.innerHTML = `<div class="list__actions">
+                    <a class="list__button">Изменить</a>
+                    <a class="list__button list__button--remove">Удалить</a>
+                </div>
+                <h5 class="list__title">${bookTitle}</h5>
+                <p class="list__subtitle">${bookAuthor}</p>`;
+        return node;
+    }
+
     validateFields() {
         return Fields.every( fieldName => this.$fields[fieldName].value != '' );
+    }
+
+    clearFields() {
+        Fields.forEach( fieldName => this.$fields[fieldName].value = '' );
     }
 
     attachEvents() {
@@ -28,7 +57,14 @@ export default class BookManager {
 
     submitHandler( e ) {
         if (this.validateFields()) {
-            // proceed
+            if (this.$fields.id.value == 0) {
+                // добавление новой книги
+                const $itemNode = this.createItemNode();
+                this.$list.appendChild( $itemNode );
+                this.clearFields();
+            } else {
+                // редактирование существующей книги
+            }
         } else {
             alert('Не все поля заполнены');
         }
